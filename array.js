68d845baf3638646582f14fd9c1b8979080de6db
HamzaSiraj456 <hamzasiraj.770@gmail.com>
2024-05-09 17:49:34 -0700

    first commit

---
+++ array.js
@@ -0,0 +1,34 @@
+
+// topic of slice Array
+
+// var fruitName=["Mango" , "Orange" ,"Apple" , "Guva" , "pineapple"];
+// var requiredArray=fruitName.slice(2,6)
+// console.log(fruitName , requiredArray)
+
+
+// Topic of related of Array
+
+var studentName =["Ali" ,"Hamza" ,"Ahsan"];
+var studentScore=[480 ,350 ,250];
+var index=0;
+console.log(index)
+console.log("score of" + index[0] + "is" + studentScore[0] + "." (studentScore[0] /500) *100 + "%")
+
+console.log(index)
+index++ 
+console.log("score of" + index[1] + "is" + studentScore[1] + "." (studentScore[1] /500) *100 + "%")
+
+console.log(index)
+index++
+console.log("score of" + index[2] + "is" + studentScore[2] + "." (studentScore[2] /500) *100 + "%")
+
+
+
+var number =[1,2,3,4,5];
+console.log(number)
+number.shift(2,3)
+console.log(number)
+var requiredArray=number.slice(3,5)
+console.log(number,requiredArray)
+
+
